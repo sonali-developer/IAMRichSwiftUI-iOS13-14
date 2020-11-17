937e8464954a5402c9767df8b46d64ea57d21fc6
SONALI PATEL <patelsonali123@yahoo.com>
2020-11-17 04:02:49 -0500

    Finished I AM Rich SwiftUI based app

---
+++ I AM Rich/I AM Rich/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  I AM Rich
+//
+//  Created by Sonali Patel on 11/17/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I AM RICH")
+                .font(.system(size: 40))
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+                    .padding()
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
